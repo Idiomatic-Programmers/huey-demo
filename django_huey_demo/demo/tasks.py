52a3ef9cea97d3b45467dd6f143698ee9065065c
Udit <23015406+udit-001@users.noreply.github.com>
2022-05-09 13:09:28 +0530

    Added exception and retry logic to task

--- django_huey_demo/demo/tasks.py
+++ django_huey_demo/demo/tasks.py
@@ -6,14 +6,17 @@ from huey.contrib.djhuey import db_periodic_task
 from demo.models import Word
 
 
-@db_periodic_task(crontab(hour="18", minute="00"))
+@db_periodic_task(crontab(hour="18", minute="00"), retries=2, retry_delay=20)
 def fetch_daily_word():
     r = requests.get(
         f"https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key={settings.WORDNIK_API_KEY}")
-    data = r.json()
-    Word.objects.create(
-        word=data["word"],
-        part_of_speech=data["definitions"][0]["partOfSpeech"],
-        definition=data["definitions"][0]["text"],
-        example=data["examples"][0]["text"]
-    )
+    if r.status_code == 200:
+        data = r.json()
+        Word.objects.create(
+            word=data["word"],
+            part_of_speech=data["definitions"][0]["partOfSpeech"],
+            definition=data["definitions"][0]["text"],
+            example=data["examples"][0]["text"]
+        )
+    else:
+        raise Exception("Unable to fetch data from Wordnik API")
